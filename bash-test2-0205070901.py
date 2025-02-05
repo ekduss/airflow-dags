9c3f209d9c8d4233f2202d991557ac312ec67aff
ekduss <dayeon@ust21.co.kr>
2025-02-05 16:09:02 +0900

    Pushed DAG bash-test2-0205070901

---
+++ bash-test2-0205070901.py
@@ -0,0 +1,31 @@
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "bash-test2-0205070901",
+}
+
+dag = DAG(
+    "bash-test2-0205070901",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `bash-test.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: {"catalog_type": "elyra-airflow-examples-catalog", "component_ref": {"component-id": "bash_operator.py"}}
+
+op_c86b1529_42c6_48e0_905e_49bef0f89226 = BashOperator(
+    task_id="bash_dy",
+    bash_command="cat /etc/hosts && echo $TEST_ENV",
+    xcom_push=False,
+    env={"TEST_ENV": "Hello World dayeon2"},
+    output_encoding="utf-8",
+    executor_config={},
+    dag=dag,
+)
